c4bafc76a72f1bfb6f4a6b1682426e32a350d891
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-25 15:55:49 +0100

    conclusione esercizio senza bonus

--- Js/script.js
+++ Js/script.js
@@ -7,3 +7,23 @@ console.log(kmPercorsi);
 
 let eta = parseInt(prompt("Inserisci la tua età:"));
 console.log(eta);
+
+// Creo una variabile per calcolare il prezzo non scontato
+
+let prezzoNonScontato = parseFloat(kmPercorsi * 0.21).toFixed(2);
+console.log(prezzoNonScontato);
+
+// Creo una condizione secondo la quale chi ha meno di 18 anni ha uno sconto del 20%, chi ha più di 65 anni ottiene uno sconto del 40% altrimenti paga il biglietto non scontato.
+
+if(eta < 18){
+    let prezzoScontatoMinorenni = "Il costo del biglietto è: "+(prezzoNonScontato * 0.8).toFixed(2)+"€";
+    console.log(prezzoScontatoMinorenni);
+
+}else if(eta > 65){
+    let prezzoScontatoSenior = "Il costo del biglietto  è: "+(prezzoNonScontato * 0.6).toFixed(2)+"€";
+    console.log(prezzoScontatoSenior);
+
+}else{
+    let prezzoFinale = "Il costo del biglietto è: "+prezzoNonScontato+"€";
+    console.log(prezzoFinale);
+};

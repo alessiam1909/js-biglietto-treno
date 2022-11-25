312c0ab5d72f2ef33c79e686a524c15972bda8ee
alessiam1909 <ale.menon.vr@gmail.com>
2022-11-25 15:14:51 +0100

    creazione prime due variabili , chiedo km ed età

---
+++ Js/script.js
@@ -0,0 +1,9 @@
+// Chiedo all'utente il numero di chilometri percorsi
+
+let kmPercorsi = parseFloat(prompt("Inserisci quanti chilometri devi percorrere:"));
+console.log(kmPercorsi);
+
+// Chiedo all'utente la sua età
+
+let eta = parseInt(prompt("Inserisci la tua età:"));
+console.log(eta);
